53e974ae32ff0d86f5a71b258bdedc39c6c07d56
Rodrigo Brasil <rodrigobbrasil@gmail.com>
2020-07-28 13:38:54 -0300

    funcao lambda de liga/desliga EC2 em Python

---
+++ lambda_function/main.py
@@ -0,0 +1,32 @@
+import sys
+import boto3
+import json
+from botocore.exceptions import ClientError
+
+regiao = 'us-east-1'
+
+def lambda_handler(event, context):
+    instancias = event.get('id')
+    operacao = event.get('op')
+
+    # '' ou None ?
+    if instancias is None or operacao is None:
+        print("Campos incompletos.")
+        return("Campos incompletos.")
+    
+    if operacao == "on":
+                try:
+                    ec2 = boto3.client('ec2', region_name=regiao)
+                    response = ec2.start_instances(InstanceIds=[instancias], DryRun=False)
+                    print(response)
+                except ClientError as e:
+                    print(e)
+                    return(e.response['Error']['Message'])
+    elif operacao == "off":
+                try:
+                    ec2 = boto3.client('ec2', region_name=regiao)
+                    response = ec2.stop_instances(InstanceIds=[instancias], DryRun=False)
+                    print(response)
+                except ClientError as e:
+                    print(e)
+                    return(e.response['Error']['Message'])
