47971ea9b764c23770c1126578807d40e886e797
Rodrigo Brasil <rodrigobbrasil@gmail.com>
2020-08-03 19:25:03 -0300

    regras CORS no S3

--- s3.tf
+++ s3.tf
@@ -5,6 +5,13 @@ resource "aws_s3_bucket" "bucket" {
   website {
     index_document = "index.html"
   }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
 }
 
 resource "aws_s3_bucket_object" "html" {
